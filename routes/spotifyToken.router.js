c1ac2034b8959fada9e065094192c53089480d11
unknown <arielgomezmendez94@gmail.com>
2026-03-31 12:18:16 -0400

    Get spotify token

--- routes/spotifyToken.router.js
+++ routes/spotifyToken.router.js
@@ -2,8 +2,44 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Get spotify token");
+let expiresAt = null;
+let token = null;
+let data;
+
+router.get("/", async (req, res) => {
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+  const base64 = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+
+  if (Date.now() > expiresAt) {
+    console.log("Rquest for new token");
+
+    //Get spotify token
+    try {
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${base64}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: "grant_type=client_credentials",
+      });
+
+      data = await response.json();
+      token = data.access_token;
+      expiresIn = data.expires_in;
+      //Get the expiration time of token
+      expiresAt = Date.now() + (expiresIn - 60) * 1000;
+
+      res.json(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Error geting Spotfy token" });
+    }
+  } else {
+    console.log("Token works");
+    res.json(data);
+  }
 });
 
 module.exports = router;
