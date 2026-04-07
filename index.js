2a352955e85718e5f940c4d2c1b28cce7ca970a3
unknown <arielgomezmendez94@gmail.com>
2026-04-07 09:29:41 -0400

    Allow dynamic port to deploy en RENDER

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const routerApi = require("./routes");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 const cors = require("cors");
