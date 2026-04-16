58c01a6c3f0e0442fd8c25ae2552f703d2ecd84f
unknown <arielgomezmendez94@gmail.com>
2026-04-16 15:50:32 -0400

    Fetch data from Deezer api

--- routes/deezerSearch.router.js
+++ routes/deezerSearch.router.js
@@ -6,7 +6,6 @@ router.get("/", async (req, res) => {
   try {
     const response = await fetch("https://api.deezer.com/search?q=nevermore");
     const data = await response.json();
-    console.log("Data: ", data);
     res.json(data)
   } catch (error) {
     console.error("Error fetching the data from Deezer");
